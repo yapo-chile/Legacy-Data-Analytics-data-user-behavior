c60fdd06d4a9f235d3a9b0b01099205176433bcb
aaron-dominguez <aaron.dominguez@adevinta.com>
2020-05-20 10:42:27 -0400

    [feat/crawl-to-db]: fixing missing libs and location

--- scrapper/app/scripts/entrypoint.sh
+++ scrapper/app/scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
-scrapy crawl pi_new_ads_venta -o ${PORTAL_INMO_NEWS_ADS_VENTA} -t csv
-scrapy crawl pi_new_ads_arriendo -o ${PORTAL_INMO_NEWS_ADS_ARRIENDO} -t csv
+cd /app/extractor/core/
+scrapy crawl pi_new_ads_venta
+scrapy crawl pi_new_ads_arriendo
