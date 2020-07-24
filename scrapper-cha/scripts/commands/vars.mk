29982b864159fc8e507184753d4dffff4cfcb2fd
aaron-dominguez <aaron.dominguez@adevinta.com>
2020-07-24 18:12:31 -0400

    [feat/cha-spider]: fixing app name

--- scrapper-cha/scripts/commands/vars.mk
+++ scrapper-cha/scripts/commands/vars.mk
@@ -16,7 +16,7 @@ export REPORT_ARTIFACTS=reports
 #APP variables
 genport = $(shell expr \( $(shell id -u) - \( $(shell id -u) / 100 \) \* 100 \) \* 200 + 30400 + $(1))
 
-export APPNAME=data-user-behavior-scrapper
+export APPNAME=data-user-behavior-scrapper-cha
 export VERSION=0.0.1
 export EXEC=./${APPNAME}
 export YO=$(shell expr `whoami`)
