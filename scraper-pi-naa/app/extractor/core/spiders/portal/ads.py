9a3f261f8c267b20131829aa2a71ddacde659d78
aaron-dominguez <aaron.dominguez@adevinta.com>
2021-06-16 11:31:10 -0400

    [fix/portal-inmo]: fixing issue direccion field

--- scraper-pi-naa/app/extractor/core/spiders/portal/ads.py
+++ scraper-pi-naa/app/extractor/core/spiders/portal/ads.py
@@ -248,7 +248,7 @@ class PISpider(scrapy.Spider):
             l['agencia'] = ''
         l['telefonos'] = set_default(response.xpath('//span[@class="profile-info-phone-value"]/text()').extract_first(), 0)
         l['constructora'] = set_default(response.css('div.info-project-constructs p.info::text').extract_first(), '')
-        l['direccion'] = clean_string(response.css('div.ui-pdp-media__body h2::text').extract_first())
+        l['direccion'] = clean_string(response.css('div.ui-vip-location div.ui-pdp-media__body p::text').extract_first())
         l['locacion'] = "{}".format(" - ".join(locations[::-1]))
         l['id'] = set_default(response.css('p.ui-vpp-denounce__info span::text').extract_first(), '')
         l['url'] = response.url
